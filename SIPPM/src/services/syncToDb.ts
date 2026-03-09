11ffce82fdb04dcb2fee4466951a5142c2d83f50
Ariq Fakhrizaki <ariqfakhrizakip@gmail.com>
2026-03-09 20:30:08 +0700

    syncing SDM data to main database

---
+++ SIPPM/src/services/syncToDb.ts
@@ -0,0 +1,49 @@
+import { apiReader, SdmResponse } from './apiReader';
+import pool from '../config/database';
+
+export class syncToDB {
+    static async syncSDM(): Promise<void> {
+        try {
+            console.log("Meminta apiReadeer untuk menarik data dari SISTER...");
+            
+            const dataSDM: SdmResponse[] = await apiReader.fetchSDM();
+
+            if(!dataSDM || dataSDM.length === 0){
+                console.log("Tidak ada data SDM yang ditarik");
+            }
+
+            console.log(`Berhasil mendapatkan ${dataSDM.length} data. Mulai menyimpan ke database...`);
+
+            let countInserted = 0;
+
+            for(const sdm of dataSDM){
+                if(!sdm.nidn || sdm.nidn.trim() === '') continue;
+                
+                const dummyEmail = `${sdm.nidn}@sister.sync`;
+                const defaultPassword = 'password123';
+
+                const query = `
+                INSERT INTO users (id, nama, nidn, email, password) 
+                VALUES (?, ?, ?, ?, ?)
+                ON DUPLICATE KEY UPDATE name = VALUES(name)
+                `;
+                await pool.execute(query, [
+                    sdm.id_sdm || "ID tidak diketahui",
+                    sdm.nama_sdm || "Nama tidak diketahui",
+                    sdm.nidn,
+                    dummyEmail,
+                    defaultPassword
+                ]);
+                
+                countInserted++;
+            }
+        } catch (error: any) {
+            console.error("Terjadi kesalahan saat sinkronisasi ke DB", error.message);
+            
+        }
+    }
+}
+
+if (require.main === module) {
+  syncToDB.syncSDM();
+}
